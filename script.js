50e924b6ea7808bb3d752fa00d8b295b60ac86f1
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-17 11:17:58 +0200

    Improve createSquares()

--- script.js
+++ script.js
@@ -10,16 +10,17 @@ let container = document.createElement("div");
 container.classList.add("container")
 body.append(container)
 
-createSquares(16)
+createSquares(100)
 
 function createSquares(sideSize) {
     let numberOfSquares = sideSize*sideSize
     for (let i = 0; i < numberOfSquares; i++) {
         let square = document.createElement("div")
         square.classList.add("square")
+        square.style.cssText += `width: ${300/sideSize}px; height: ${300/sideSize}px`
         container.appendChild(square)
         square.addEventListener("mouseover", () => {
-            square.style.cssText = "background: brown;"
+            square.style.cssText += "background: brown;"
         })
     }
 }
