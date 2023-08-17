4d17bbe5c8388704eca20f056d4aa85d500218cb
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-17 11:49:22 +0200

    Add style and improve createSquares()

--- script.js
+++ script.js
@@ -10,9 +10,13 @@ let container = document.createElement("div");
 container.classList.add("container")
 body.append(container)
 
-createSquares(100)
+createSquares(10)
 
 function createSquares(sideSize) {
+    let squaresToRemove = document.querySelectorAll(".square");
+    squaresToRemove.forEach(square => {
+        container.removeChild(square);
+    });
     let numberOfSquares = sideSize*sideSize
     for (let i = 0; i < numberOfSquares; i++) {
         let square = document.createElement("div")
@@ -20,7 +24,16 @@ function createSquares(sideSize) {
         square.style.cssText += `width: ${300/sideSize}px; height: ${300/sideSize}px`
         container.appendChild(square)
         square.addEventListener("mouseover", () => {
-            square.style.cssText += "background: brown;"
+            let hexString = Math.round(Math.random()*4294967296/100000)*100000;
+            hexString = hexString.toString(16);
+            //debugger;
+            square.style.cssText += `background: #${hexString};`
         })
     }
 }
+
+
+button.addEventListener("click", () => {
+    let numberOfSquares = prompt("Number of squares (side): ", 16);
+    numberOfSquares > 100 ? alert("Number should be lower than 101") : createSquares(numberOfSquares); 
+})
